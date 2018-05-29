36b54f14da37669251a7fc84970fc385b314da24
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-29 09:59:22 -0300

    Integrando o React-Redux (Parte 2) - transformando componente em react-redux

--- ex/counter.jsx
+++ ex/counter.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+/* transformar este componente puramente react em react-redux */
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { inc, dec, stepChanged } from './counterActions'
+
 const Counter = props => (
     <div>OK</div>
 )
 
-export default Counter
+const mapStateToProps = state => ({ counter: state.counter })
+const mapDispatchToProps = dispatch => bindActionCreators({ inc, dec, stepChanged }, dispatch)
+
+
+/* o resultado de "connect(mapStateToProps, mapDispatchToProps)"
+retorna uma função que recebe "(Counter)" e essa função e exportada por "export default"
+*/
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
