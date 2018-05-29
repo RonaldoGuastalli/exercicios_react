1f7b60121cbfce5f968cd6a8d05b6ae17d134c21
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-29 09:16:50 -0300

    Integrando o React-Redux (Parte 1) - Action Creators

---
+++ ex/counterActions.js
@@ -0,0 +1,18 @@
+/* 
+Action Creator - 3 funções
+*/
+
+export function inc(){
+    return {type: 'INC'}
+}
+
+export function dec(){
+    return {type: 'DEC'}
+}
+
+export function stepChange(e){
+    return {
+        type: 'STEP_CHANGED',
+        payload: e.target.value
+    }
+}
